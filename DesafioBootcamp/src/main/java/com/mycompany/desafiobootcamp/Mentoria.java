205a0c6ec1de98c92904cde2f12758f0482fe8c4
CarlosEduardo-067 <carloseduardodossantosvital@gmail.com>
2024-06-03 17:42:13 -0400

    Desafio bootcamp classes

---
+++ DesafioBootcamp/src/main/java/com/mycompany/desafiobootcamp/Mentoria.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.desafiobootcamp;
+
+import java.time.LocalDate;
+/**
+ *
+ * @author carlo
+ */
+
+
+public class Mentoria extends Conteudo{
+
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public Mentoria() {
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
